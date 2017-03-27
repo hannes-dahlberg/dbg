6b482ff9bb400cdc456519c03081ec6acfe5231f
Hannes Dahlberg <hannes@dwik.se>
2017-03-27 12:07:38 +0200

    Blade template to include js app, css app and basic headers. No more blade template will be necessary

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" type="text/css" href="/css/app.css" />
+        <title>DBG</title>
+    </head>
+    <body>
+        <div id="app">
+
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
