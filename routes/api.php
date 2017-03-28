77a0e7f91e3eb067b14c950a4848bfce82656e68
Hannes Dahlberg <hannes@dwik.se>
2017-03-28 09:08:19 +0200

    API routes for accounts, budget and transactions

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,11 @@ Route::group(['namespace' => 'API'], function() {
         Route::post('/', ['name' => 'auth', 'uses' => 'AuthController@Auth']);
     });
 
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('/user', function(Request $request) { return $request->user(); });
+
+        Route::resource('account', AccountController::class, ['except' => ['create', 'show', 'edit']]);
+        Route::resource('budget', BudgetController::class, ['except' => ['create', 'show', 'edit']]);
+        Route::resource('transaction', TransactionController::class, ['except' => ['create', 'show', 'edit']]);
+    });
 });
