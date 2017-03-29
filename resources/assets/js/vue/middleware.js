157d7b69e6ca6b34a49d7b429b4ad7095914c2a4
Hannes Dahlberg <hannes@dwik.se>
2017-03-29 10:10:40 +0200

    Updated middleware and added guest middleware

--- resources/assets/js/vue/middleware.js
+++ resources/assets/js/vue/middleware.js
@@ -4,6 +4,9 @@ export default {
     //Middleware for authenticated users
     auth(to, from, next) {
         //Users not authenticated will be redirected to 404
-        next(!store.getters.isAuth ? { name: 'error.404' } : undefined);
+        next(!store.getters.isAuth ? { name: 'auth.login' } : undefined);
+    },
+    guest(to, from, next) {
+        next(store.getters.isAuth ? { name: 'error.404' } : undefined);
     }
 }
